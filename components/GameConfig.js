23338fb257c88eca9fabdd6e11f0ce57836a8fc2
alkorgli-B <alkorgli26@gmail.com>
2026-02-13 20:57:56 +0200

    Create GameConfig.js

---
+++ components/GameConfig.js
@@ -0,0 +1,17 @@
+export const GAME_CONFIG = {
+  CAT_SPEED: 0.1,
+  CRYSTAL_COUNT: 5,
+  COLORS: {
+    PLAYER: "#00f3ff",
+    EMISSIVE: "#0066ff",
+    CRYSTAL: "#ff0077",
+    GRID: "#00f3ff"
+  },
+  CRYSTAL_POSITIONS: [
+    [5, 0.6, -5],
+    [-8, 0.6, 2],
+    [0, 0.6, 8],
+    [7, 0.6, 4],
+    [-5, 0.6, -7]
+  ]
+};
